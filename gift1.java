d90ee8dab52dd9a3f60655340eab6906ee6d32b2
Deepansha <36552319+MaTh-PhYsIcS-GiRl@users.noreply.github.com>
2018-09-19 17:30:47 -0400

    Add files via upload

---
+++ gift1.java
@@ -0,0 +1,15 @@
+/**
+ * ID : deepans9
+ * LANG : JAVA
+ * Task : gift1
+ */
+import java.io.*;
+import java.util.*;
+public class gift1
+{
+	
+	public static void main(String[] args)
+	{
+		
+	}
+}
